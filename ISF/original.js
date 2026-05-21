c3f00ecc9cfa60b220157ccec527b3b90292668a
ObiloxYT <AfriaStudios@outlook.in>
2026-05-21 11:48:05 +0530

    Implement ISF class with queue and tick methods

---
+++ ISF/original.js
@@ -0,0 +1,13 @@
+class ISF {
+static queued=[]
+static queue(yieldFn){
+ISF.queued.push(yieldFn())
+}
+static tick(){
+while(ISF.queued[0]&&!api.isNearInterrupt()){
+if(ISF.queued[0].next().done)ISF.queued.shift()
+}
+}
+}
+globalThis.ISF=ISF;
+tick=ISF.tick
